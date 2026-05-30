b8347b8d8c1d2082ab9a128adf2c85ace067770a
Rodel Gerodias <rodelgerodias25@gmail.com>
2026-05-30 16:56:44 +0800

    feat: add route and navigation for messages

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('chat', 'chat')->name('chat');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
