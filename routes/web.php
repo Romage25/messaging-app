573bb0c8457f77023deddce2e02e028255ce657d
Rodel Gerodias <rodelgerodias25@gmail.com>
2026-06-05 14:25:57 +0800

    feat: add test mail route and navigation

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::view('/', 'welcome')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
     Route::view('chat', 'chat')->name('chat');
+    Route::view('mail', 'mail')->name('mail');
 });
 
 require __DIR__ . '/settings.php';
